d4f1136475f80a521c7084402ce30a262adfe96c
Nodirbek <nodib7034@gmail.com>
2020-07-09 01:16:57 +0200

    added CRUD interface

---
+++ models/genre.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var genreSchema = new Schema({
+    name: {type: String, minlength: 3, maxlength: 100}
+
+});
+
+genreSchema
+.virtual('url')
+.get(function(){
+    return '/catalog/genre/' + this._id;
+});
+
+module.exports = mongoose.model('Genre', genreSchema);
